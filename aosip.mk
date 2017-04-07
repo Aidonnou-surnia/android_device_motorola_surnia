6c24ed1d84f7a534c47741e83e418184b3e6d155
ElDainosor <eldainosor@gmail.com>
2017-04-07 03:16:10 +0000

    AOSiP Bringup
    Prepare for teh owl

--- aosip.mk
+++ aosip.mk
@@ -15,16 +15,27 @@
 
 $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
+# Inherit common product files.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
 # Boot animation
-TARGET_SCREEN_WIDTH := 540
-TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Telephony-ext
+ PRODUCT_PACKAGES += telephony-ext
+ PRODUCT_BOOT_JARS += telephony-ext
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := aosip_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd gen)"
+
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Wasteland (ElDainosor)"
+
